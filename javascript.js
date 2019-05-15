caca531991a39c456e74f3354cde09c2e877db0e
elih43 <50376763+elih43@users.noreply.github.com>
2019-05-15 11:13:22 -0500

    added passing periods to friday

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var count = 0;
-var hours = 14;
-var minutes = 10;
+var hours = 10;
+var minutes = 58;
 var seconds = 0;
 var period;
 var passing = 0;
@@ -72,8 +72,9 @@ function currentPeriod() {
 
     var currentDay = currentDayOfWeek.getDay();
 
+    //mon-thurs schedule
     if (currentDay < 5 && currentDay > 0) {
-        //mon-thurs schedule
+
         if ((hours == 7 && minutes >= 45) || (hours == 8 && minutes < 36)) {
             period = 1;
             setTime(8, 36);
@@ -110,17 +111,13 @@ function currentPeriod() {
             period = "passing";
             setTimePassing(12, 20);
             pp = true;
-
-
         } else if ((hours == 12 && minutes >= 20) || (hours == 13 && minutes < 11)) {
             period = 6
             setTime(13, 11);
-
         } else if (((hours == 13 && minutes >= 11) && minutes < 15) || (minutes > 11 && (hours == 13 && minutes < 15))) {
             period = "passing";
             setTimePassing(13, 15);
             pp = true;
-
         } else if ((hours == 13 && minutes >= 15) || (hours == 14 && minutes < 6)) {
             period = 7;
             setTime(14, 6);
@@ -128,13 +125,11 @@ function currentPeriod() {
             period = "passing";
             setTimePassing(14, 10);
             pp = true;
-
         } else if ((hours == 14 && minutes >= 10) || (hours == 15 && minutes < 5)) {
             period = 8;
             setTime(15, 5);
         } else {
-            pp = true;
-            period = "Passing";
+            //
 
         }
 
@@ -143,14 +138,24 @@ function currentPeriod() {
             document.getElementById("label-period").innerHTML = "Period: No School";
 
 
-        } else if (pp) {
-            document.getElementById("label-period").innerHTML = "Period: " + period;
+        }
+        //passing period
+        else if (pp) {
+            if (time >= 0 && passing == 1) {
+                passing = 0;
+            }
+            if (passing >= 0 && time == 1) {
+                time = 0;
+            }
 
+            document.getElementById("label-period").innerHTML = "Period: " + period;
             document.getElementById("time-remainLabel").innerHTML = "Minutes left in passing: " + passing;
 
 
 
-        } else {
+        }
+        //in period
+        else if (!pp) {
 
             if (time >= 0 && passing == 1) {
                 passing = 0;
@@ -164,39 +169,72 @@ function currentPeriod() {
 
 
         }
-    } else if (currentDay == 5) {
-        //friday schedule
+    }
+
+    //friday schedule
+    else if (currentDay == 5) {
+
         if ((hours == 7 && minutes >= 45) || (hours == 8 && minutes < 27)) {
             period = 1;
-            time = 60 * (8 - hours) + 27 - minutes;
+            setTime(8, 27);
+        } else if (((hours == 7 && minutes >= 27) && minutes < 31) || (minutes > 27 && (hours == 7 && minutes < 31))) {
+            period = "passing";
+            setTimePassing(7, 31);
+            pp = true;
         } else if (((hours == 8 && minutes >= 31) && minutes < 41) || (minutes > 31 && (hours == 8 && minutes < 41))) {
             period = "advisory"
-            time = 60 * (8 - hours) + 41 - minutes;
+            setTime(8, 41);
+        } else if (((hours == 8 && minutes >= 41) && minutes < 45) || (minutes > 41 && (hours == 8 && minutes < 45))) {
+            period = "passing";
+            setTimePassing(8, 45);
+            pp = true;
         } else if ((hours == 8 && minutes >= 45) || (hours == 9 && minutes < 27)) {
             period = 2;
-            time = 60 * (9 - hours) + 27 - minutes;
+            setTime(9, 27);
+        } else if (((hours == 9 && minutes >= 27) && minutes < 31) || (minutes > 27 && (hours == 9 && minutes < 31))) {
+            period = "passing";
+            setTimePassing(9, 31);
+            pp = true;
         } else if ((hours == 9 && minutes >= 31) || (hours == 10 && minutes < 13)) {
             period = 3;
-            time = 60 * (10 - hours) + 13 - minutes;
-        } else if ((hours == 10 && minutes >= 17) || (hours == 10 && minutes < 59)) {
+            setTime(10, 13);
+        } else if (((hours == 10 && minutes >= 13) && minutes < 17) || (minutes > 13 && (hours == 10 && minutes < 17))) {
+            period = "passing";
+            setTimePassing(10, 17);
+            pp = true;
+        } else if (((hours == 10 && minutes >= 17) && minutes < 59) || (minutes > 17 && (hours == 10 && minutes < 59))) {
             period = 4;
-            time = 60 * (10 - hours) + 59 - minutes;
+            setTime(10, 59);
+        } else if (((hours == 10 && minutes >= 59)) || ((hours == 11 && minutes < 3))) {
+            period = "passing";
+            setTimePassing(11, 3);
+            pp = true;
         } else if ((hours == 11 && minutes >= 3) || (hours == 11 && minutes < 45)) {
             period = 5;
-            time = 60 * (11 - hours) + 45 - minutes;
+            setTime(11, 45);
+        } else if (((hours == 11 && minutes >= 45) && minutes < 49) || (minutes > 45 && (hours == 11 && minutes < 49))) {
+            period = "passing";
+            setTimePassing(11, 49);
+            pp = true;
         } else if ((hours == 11 && minutes >= 49) || (hours == 12 && minutes < 31)) {
             period = 6
-            time = 60 * (12 - hours) + 31 - minutes;
+            setTime(12, 31);
+        } else if (((hours == 12 && minutes >= 31) && minutes < 35) || (minutes > 31 && (hours == 12 && minutes < 35))) {
+            period = "passing";
+            setTimePassing(12, 35);
+            pp = true;
         } else if ((hours == 12 && minutes >= 35) || (hours == 13 && minutes < 17)) {
             period = 7;
-            time = 60 * (13 - hours) + 17 - minutes;
+            setTime(13, 17);
+        } else if (((hours == 13 && minutes >= 17) && minutes < 21) || (minutes > 17 && (hours == 13 && minutes < 21))) {
+            period = "passing";
+            setTimePassing(13, 21);
+            pp = true;
         } else if ((hours == 13 && minutes >= 21) || (hours == 14 && minutes < 7)) {
             period = 8;
-            time = 60 * (14 - hours) + 7 - minutes;
+            setTime(14, 7);
         } else {
-            pp = true;
-            period = "Passing";
-
+            //
 
         }
 
@@ -205,18 +243,38 @@ function currentPeriod() {
             document.getElementById("label-period").innerHTML = "Period: No School";
 
 
-        } else {
+        }
+        //passing period
+        else if (pp) {
+            if (time >= 0 && passing == 1) {
+                passing = 0;
+            }
+            if (passing >= 0 && time == 1) {
+                time = 0;
+            }
+
+            document.getElementById("label-period").innerHTML = "Period: " + period;
+            document.getElementById("time-remainLabel").innerHTML = "Minutes left in passing: " + passing;
+
+
+
+        }
+        //in period
+        else if (!pp) {
+
+            if (time >= 0 && passing == 1) {
+                passing = 0;
+            }
+            if (passing >= 0 && time == 1) {
+                time = 0;
+            }
+
             document.getElementById("label-period").innerHTML = "Period: " + period;
             document.getElementById("time-remainLabel").innerHTML = "Minutes left in period: " + time;
 
 
+        } else if (currentDay > 5) {
+            //weekend
         }
-
-
-    } else if (currentDay > 5) {
-        //weekend
     }
-
-
-
 }
